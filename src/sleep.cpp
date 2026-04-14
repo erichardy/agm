5320aa9546ce76ed0a649330ee2691bcd5bbc5b2
Eric Hardy <eric.hardy.29@gmail.com>
2026-04-15 00:30:36 +0200

    tests deepsleep for ESP32-WROOM32

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -1,7 +1,7 @@
 #include <esp_sleep.h>
 
 // sleepTime_us : _us == microsecondes
-int goToBed(u_int64_t sleepTime_us) {
+int goToBed(uint64_t sleepTime_us) {
     esp_sleep_enable_timer_wakeup(sleepTime_us);
     // Serial.flush();
     esp_deep_sleep_start();
